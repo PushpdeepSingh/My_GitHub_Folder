da5adee67fe6ac78234562c54029ad577c902517
Pushpdeep Singh <psingh32@pride.hofstra.edu>
2025-04-03 13:54:07 -0400

    midterm draft2

--- Midterm copy/script.js
+++ Midterm copy/script.js
@@ -1,5 +1,5 @@
 const colors = ["#fdf6f0", "#fff7e6", "#f5fff5"];
-const userName = prompt("Welcome to Milano Bean! What's your name?");
+const userName = prompt("Welcome to Fern and Foam! What's your name?");
 const colorChoice = prompt("Pick a background color: 0 for cream, 1 for latte, 2 for mint");
 const userInfo = {
   name: userName,
